bb6bb5c0acdfabfd62ae65a0de144efa554a1ace
Polina <alllrrw@gmail.com>
2026-01-16 12:38:57 +0700

    feat: add header

---
+++ src/assets/components/Header/Header.tsx
@@ -0,0 +1,26 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header__logo">
+                <a href="https://github.com/linawashere">
+                    <img src="src\assets\images\photo_2025-07-10_13-41-10.jpg" alt="logo" />
+                </a>
+            </div>
+            <nav>
+                <ul className="header__nav">
+                    <li>
+                        <a href="#skills">Skills</a>
+                    </li>
+                    <li>
+                        <a href="#codeexample">Code example</a>
+                    </li>
+                    <li>
+                        <a href="#expirience">Expirience</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
